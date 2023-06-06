f47ca9140fdcad4050373aa0496059c8c30eddef
Vikram Singh <vs125609@gmail.com>
2023-06-06 06:44:40 +0530

    People is not working

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { Builder, By } = require('selenium-webdriver');
+const { Builder, By, Key } = require('selenium-webdriver');
 require("chromedriver");
 require('dotenv').config('');
 
 const array = ['Software Developer at Paytm', 'Software Developer at Razorpay', 'Software Developer at Cred'];
 
-(async function helloSelenium() {
+async function automation() {
 	let driver = await new Builder().forBrowser('chrome').build();
 
 	await driver.get('https://www.linkedin.com');
@@ -24,6 +24,18 @@ const array = ['Software Developer at Paytm', 'Software Developer at Razorpay', 
 	let submitButton = await driver.findElement(By.className('sign-in-form__submit-btn--full-width'));
 
 	await submitButton.click();
+
+	let Query = await driver.findElement(By.className('search-global-typeahead__input'));
+
+	await Query.sendKeys(array[0]);
+
+	await driver.actions().keyDown(Key.ENTER).perform();
+
+	// let People = await driver.findElement(By.xpath('//*[@id="search-reusables__filters-bar"]/ul/li[1]/button'));
+
+	// await People.click();
+
+
 	// The functionality of the both the statements are same on line number 14 & 16
 
 	// await driver.manage().setTimeouts({ implicit: 100000 });
@@ -32,4 +44,10 @@ const array = ['Software Developer at Paytm', 'Software Developer at Razorpay', 
 		await driver.quit();
 	}, 500000)
 
-})();
+};
+
+automation().then((data) => {
+	console.log(data);
+}).catch((error) => {
+	console.log(error);
+});
